c02cf178bcaa8c410ff6ccd544d7a74c92fe0595
olgamati <olgailjina88@gmail.com>
2022-08-25 00:30:18 +0200

    Added the Github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://olga-plotnikova.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Olga Plotnikova
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/olgamati/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
