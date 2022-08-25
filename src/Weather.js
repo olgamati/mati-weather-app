7f02224c1721189d7784539cd51b770f5010c94d
olgamati <olgailjina88@gmail.com>
2022-08-26 00:06:55 +0200

    Changed the props of city

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -64,8 +64,8 @@ export default function Weather() {
     );
   } else {
     let apiKey = "d4afb93d00c44ec28d48b4b8002244c8";
-    let city = "London";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading";
