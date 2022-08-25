2a31e92002995f1a1b6605698d82b7d5d9824ccc
olgamati <olgailjina88@gmail.com>
2022-08-25 22:15:16 +0200

    Added basic html and styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://olga-plotnikova.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Olga Plotnikova
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/olgamati/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://olga-plotnikova.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Olga Plotnikova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/olgamati/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
