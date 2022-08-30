c9a4f4562dde39d6eba69632ee3552323b2e7802
olgamati <olgailjina88@gmail.com>
2022-08-30 19:37:32 +0200

    Changed the default city

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Kyiv" />
         <footer>
           This project is coded by{" "}
           <a
